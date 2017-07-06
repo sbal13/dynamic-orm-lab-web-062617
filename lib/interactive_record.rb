4783d5a8994cd9f06a765ea8ce1088173b8e13d5
sbal13 <sbal13@gmail.com>
2017-07-05 22:01:07 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,60 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
+
+	def self.table_name
+		self.to_s.downcase.pluralize
+	end
+
+	def self.column_names
+		DB[:conn].results_as_hash = true
+
+		sql = "PRAGMA table_info(#{table_name});"
+
+		table_info = DB[:conn].execute(sql)
+
+		table_info.map{|row| row["name"]}.compact
+	end
+
+	def initialize(options={})
+    	options.each do |property, value|
+    		self.send("#{property}=", value)
+    	end
+ 	end
+
+	def save
+		sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+		DB[:conn].execute(sql)
+		@id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+	end
+
+	def table_name_for_insert
+		self.class.table_name
+	end
+
+	def values_for_insert
+		values = []
+		self.class.column_names.each do |col_name|
+			values << "'#{send(col_name)}'" unless send(col_name).nil?
+		end
+		values.join(", ")
+	end
+
+	def col_names_for_insert
+		self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+	end
+
+	def self.find_by_name(name)
+		sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+		DB[:conn].execute(sql)
+	end
+
+	def self.find_by(param)
+		sql = "SELECT * FROM #{self.table_name} WHERE #{param.keys[0]} = '#{param.values[0]}'"
+		DB[:conn].execute(sql)
+	end
+
   
 end
